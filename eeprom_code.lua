57c28a810350922eda0aa2718415564a7f89d8f2
Wowzaa89 <121795435+Wowzaa89@users.noreply.github.com>
2025-11-09 14:05:38 -0600

    Remove GPU resolution binding
    Remove hardcoded GPU resolution setting.

--- eeprom_code.lua
+++ eeprom_code.lua
@@ -3,12 +3,9 @@ local computer = computer or require('computer')
 
 local gpu = component.list("gpu")()
 local screen = component.list("screen")()
-local state = 0
-local text = "YOU'VE BEEN HACKED IDIOT!!! HAHAHAHHA!"
 
-if not gpu or not screen then error("YOU'VE BEEN HACKED IDIOT!!! HAHAHAHHA!") end
+if not gpu or not screen then error("Screen/GPU not attached.") end
 invoke(gpu, "bind", screen)
-invoke(gpu, "setResolution", 50,16)
 
 local resx, resy = invoke(gpu, "getResolution")
 local startx = (resx / 2 - #text / 2) + 1
@@ -21,20 +18,7 @@ function Sleep(timeout)
   until computer.uptime() >= deadline
 end
 
-while true do
-  if state == 0 then
-    invoke(gpu, "setBackground", 0x000000)
-    invoke(gpu, "setForeground", 0xffffff)
-    state = 1
-  else
-    invoke(gpu, "setBackground", 0xffffff)
-    invoke(gpu, "setForeground", 0x000000)
-    state = 0
-  end
-
-  invoke(gpu, "fill", 1,1, resx,resy, " ")
-  invoke(gpu, "set", startx, starty, text)
-  computer.beep()
-
-  Sleep(0.25)
-end
+invoke(gpu, "setBackground", 0x000000)
+invoke(gpu, "setForeground", 0xffffff)
+invoke(gpu, "fill", 1,1, resx,resy, " ")
+invoke(gpu, "set", startx, starty, "Booting..")
